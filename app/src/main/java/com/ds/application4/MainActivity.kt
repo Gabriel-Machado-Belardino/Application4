99292c30d8229493d4c43bd0ab981090c78eec0b
Gabriel-Machado-Belardino <gm.belardino@hotmail.com>
2022-05-31 14:16:26 -0300

    Project Complete

--- app/src/main/java/com/ds/application4/MainActivity.kt
+++ app/src/main/java/com/ds/application4/MainActivity.kt
@@ -3,9 +3,10 @@ package com.ds.application4
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.AlarmClock.EXTRA_MESSAGE
 import android.view.View
 import android.widget.EditText
+
+const val EXTRA_MESSAGE = "com.ds.application4.MESSAGE"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
